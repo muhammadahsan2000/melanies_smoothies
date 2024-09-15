9aa444e6b6f308b1fe3ea77321f350a8cc906db0
muhammadahsan <117234971+muhammadahsan2000@users.noreply.github.com>
2024-09-15 07:21:07 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,8 @@ if ingredients_list:
             values ('""" + ingredients_string + """','"""+ name_on_order +"""')"""
     
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response.json())
+    #st.text(fruityvice_response.json())
+    st.dataframe(data=fruityvice_response.json(),  use_container_width=True)
 
     time_to_insert=st.button('submit order')
     if time_to_insert:
